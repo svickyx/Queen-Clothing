7ad0ff67ada17cea7344685e1cfea2f5912413bb
静赵 <vicky@jingzhaodeMacBook-Pro.local>
2020-08-26 16:18:23 +0800

    Updates

---
+++ precache-manifest.ec1be913c31b9d76c5cb0b5abdc99360.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b90a7bfb85e703f6be17310885e121a",
+    "url": "/Queen-Clothing/index.html"
+  },
+  {
+    "revision": "166dd95ec66b99f2d4d1",
+    "url": "/Queen-Clothing/static/css/main.4a48dd39.chunk.css"
+  },
+  {
+    "revision": "26ed7263d59e481d298c",
+    "url": "/Queen-Clothing/static/js/2.91017f7e.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/Queen-Clothing/static/js/2.91017f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166dd95ec66b99f2d4d1",
+    "url": "/Queen-Clothing/static/js/main.0bfdab7f.chunk.js"
+  },
+  {
+    "revision": "98423d593ca6e7669288",
+    "url": "/Queen-Clothing/static/js/runtime-main.d96df6dc.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Queen-Clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Queen-Clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
